83b0b4c93dca13c2e0f7daaabcb98bd3cbe88c04
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-03-18 03:49:32 +0100

    test pr comment

--- tf/main.tf
+++ tf/main.tf
@@ -9,3 +9,21 @@ resource "random_string" "resource_suffix" {
 resource "aws_s3_bucket" "static_landing" {
   bucket = "notifycal-landing-page-${random_string.resource_suffix.result}"
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.static_landing.id
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::${aws_s3_bucket.static_landing.id}/*"
+        }
+      ]
+    }
+  )
+}
