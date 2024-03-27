651a0b5422501635c41a15de259b58b2fdd6e3af
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-03-28 00:10:18 +0100

    wip

--- tf/main.tf
+++ tf/main.tf
@@ -1,13 +1,13 @@
 resource "random_string" "resource_suffix" {
-  length = 5
-  lower = true
-  upper = false
+  length  = 5
+  lower   = true
+  upper   = false
   numeric = true
   special = false
 }
 
 module "static_landing" {
-  source = "../../modules/tofu-module-static-website"
+  source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v0.1.1"
 
   bucket_name = "notifycal-landing-page-${random_string.resource_suffix.result}"
 }
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,7 @@
-output bucket_name {
+output "bucket_name" {
   value = module.static_landing.bucket_name
 }
 
-output site_url {
+output "site_url" {
   value = module.static_landing.site_url
 }
