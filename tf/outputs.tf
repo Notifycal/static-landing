3bbd382b9cdaaf99839c975aea6675575efee3c8
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-03-27 23:29:03 +0100

    wip: use static-site module

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,7 @@
 output bucket_name {
-  value = aws_s3_bucket.static_landing.id
+  value = module.static_landing.bucket_name
 }
 
 output site_url {
-  value = "http://${aws_s3_bucket_website_configuration.this.website_endpoint}"
+  value = module.static_landing.site_url
 }
