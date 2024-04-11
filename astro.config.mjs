cd5c124bf7c6f62680c5be8cf69c584e6114198d
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2024-04-12 01:05:07 +0200

    feat: i18n (#20)
    - `/` -> for the default language (`en` currently), no redirect.
    - `/es/` -> spanish version.
    - `/en/` -> `404`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 import partytown from '@astrojs/partytown';
 
+import { defaultLang, hideDefaultLang, languages } from './src/i18n/ui';
+
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: defaultLang,
+    locales: Object.keys(languages),
+    routing: {
+      prefixDefaultLocale: !hideDefaultLang
+    }
+  },
   integrations: [
     tailwind(),
     partytown({
---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  en: 'English',
+  es: 'Espa&ntilde;ol'
+};
+
+export const defaultLang = 'en';
+
+export const hideDefaultLang = true;
+
+export const ui = {
+  en: {
+    hero: 'Welcome to Astro',
+    subhero:
+      'To get started, open the directory src/pages in your project. Code Challenge: Tweak the "Welcome to Astro" message above. '
+  },
+  es: {
+    hero: 'Ola ke ase!',
+    subhero: 'Holisss!'
+  }
+} as const;
