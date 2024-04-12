c61080810e0a2d3a90ffbef3131c223eea65c210
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-04-12 23:53:13 +0200

    chore: do language at component level without props

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from 'daisyui';
+import themes from 'daisyui/src/theming/themes';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {}
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
+    themes: [
+      {
+        light: {
+          ...themes.light,
+          primary: '#5ad529'
+          // 'primary-content': '#FFF',
+          // secondary: ''
+        }
+      }
+    ],
     darkTheme: 'light' // don't want any dark themes
   }
 };
