dee85b7a98e3102445951d3432e4a0b5414f2b06
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-05-01 21:46:37 +0200

    feat: do language redirections

--- tf/main.tf
+++ tf/main.tf
@@ -1,8 +1,31 @@
+data "cloudflare_zone" "main" {
+  name = var.base_domain
+}
+
 module "static_landing" {
   source = "git@github.com:Notifycal/tofu-module-static-website.git?ref=v1.0.1"
 
-  bucket_name = var.main_site_domain
+  bucket_name = local.domain
   
   enable_s3_public_access = false
-  enable_www_redirect = true
+  enable_www_redirect = var.enable_www_redirect
+}
+
+resource "cloudflare_record" "main" {
+  zone_id = data.cloudflare_zone.main.id
+  # @ is how Cloudflare calls the naked domain
+  name    = local.main_dns_record_name
+  value   = module.static_landing.site_urls.main
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_record" "www_redirect" {
+  count = var.enable_www_redirect ? 1 : 0
+
+  zone_id = data.cloudflare_zone.main.id
+  name    = local.redirect_dns_record_name
+  value   = module.static_landing.site_urls.redirect
+  type    = "CNAME"
+  proxied = true
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,5 +1,10 @@
-variable "main_site_domain" {
+variable "base_domain" {
   type = string
+}
+
+variable "domain_prefix" {
+  type = string
+  default = ""
 }
 
 variable "enable_www_redirect" {
@@ -8,7 +13,15 @@ variable "enable_www_redirect" {
   default = true
 }
 
-variable "redirect_domains" {
+variable "redirect_base_domains" {
   type = list(string)
   default = []
+}
+
+locals {
+  domain = var.domain_prefix == "" ? var.base_domain : "${var.domain_prefix}.${var.base_domain}"
+  redirect_domains = { for redirect_domain in var.redirect_base_domains: redirect_domain => var.domain_prefix == "" ? redirect_domain : "${var.domain_prefix}.${redirect_domain}" }
+
+  main_dns_record_name = var.domain_prefix == "" ? "@" : var.domain_prefix
+  redirect_dns_record_name = var.domain_prefix == "" ? "www" : "www.${var.domain_prefix}"
 }
