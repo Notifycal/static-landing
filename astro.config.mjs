96b37f6eef2a84f90fb0b5f0ab86a72a5fa9dc8e
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-04 23:14:02 +0200

    drop bundle size plugin, it is reporting incorrect sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,8 @@ import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 import { defaultLang, hideDefaultLang, languages } from './src/i18n/ui';
 
-import { bundleSizePlugin } from '@notifycal/shared/utils';
-
 import playformCompress from '@playform/compress';
 import tailwindcss from '@tailwindcss/vite';
-
-const maxBundleChunkSizeInBytes = 4.2 * 1024 * 1024; // MB
-const maxTotalBundleSizeInBytes = 4.3 * 1024 * 1024; // MB
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,13 +27,6 @@ export default defineConfig({
     playformCompress()
   ],
   vite: {
-    plugins: [tailwindcss()],
-    build: {
-      reportCompressedSize: true,
-      chunkSizeWarningLimit: (maxBundleChunkSizeInBytes / 1024) * 0.9, // Expressed in KB. The budget is 90% of the limit.
-      rollupOptions: {
-        plugins: [bundleSizePlugin(maxBundleChunkSizeInBytes, maxTotalBundleSizeInBytes)]
-      }
-    }
+    plugins: [tailwindcss()]
   }
 });
