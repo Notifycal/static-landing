fbb811c931a8babe052ee40808e5b38334b104d8
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-08-04 23:19:01 +0200

    feat: react components use one from shared (#359)
    Co-authored-by: dsiguero <26867647+dsiguero@users.noreply.github.com>

---
+++ src/components/TierSelection.tsx
@@ -0,0 +1,55 @@
+import { Container, MantineProvider } from '@mantine/core';
+import { TierSelection as TierSelectionReact } from '@notifycal/shared/components';
+import { orderedTierInfoWithIcons } from '@notifycal/shared/pricing';
+import type { LanguageCode } from '@notifycal/shared/types';
+
+interface TierSelection {
+  locale: LanguageCode;
+}
+
+export function TierSelection({ locale }: TierSelection) {
+  const tiers: Parameters<typeof orderedTierInfoWithIcons>[0] = {
+    good: {
+      name: 'Good Plan',
+      credits: 100,
+      priceEur: 10,
+      priceId: 'good',
+      productId: 'good',
+      numberOfReminders: 100
+    },
+    better: {
+      name: 'Better Plan',
+      credits: 350,
+      priceEur: 25,
+      priceId: 'better',
+      productId: 'better',
+      numberOfReminders: 350
+    },
+    best: {
+      name: 'Best Plan',
+      credits: 1000,
+      priceEur: 60,
+      priceId: 'best',
+      productId: 'best',
+      numberOfReminders: 1000
+    }
+  };
+
+  return (
+    <MantineProvider>
+      <Container>
+        <TierSelectionReact
+          isCardButtonDisabled={() => false}
+          isCardButtonLoading={() => false}
+          lang={locale}
+          orderedTierInfoWithIcons={orderedTierInfoWithIcons(tiers, locale)}
+          onTierSelection={(tierId) => {
+            console.log(`User clicked on tier ${tierId}`);
+            window.open('https://privatedev.notifycal.com', '_blank');
+            return Promise.resolve();
+          }}
+        />
+      </Container>
+    </MantineProvider>
+  );
+}
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,6 +1,7 @@
+import type { LanguageCode } from '@notifycal/shared/types';
 import { defaultLang, ui } from './ui';
 
-export function getLangFromUrl(url: URL) {
+export function getLangFromUrl(url: URL): LanguageCode {
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
