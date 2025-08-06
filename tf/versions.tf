9e37b04156fb5fe7fed76d1c069854fcb01b5532
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-06 11:30:32 +0200

    follow TF guidelines on module provider versions

--- tf/versions.tf
+++ tf/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = ">= 6.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.8"
+      version = ">= 5.8"
     }
   }
 }
