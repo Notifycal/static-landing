9af0f7193f7a36a669f81015af7ee3f9bfca9842
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-08-18 19:29:05 +0200

    fix spacing between text and make them equal height

--- src/layouts/function-components/TestimonialSlider.tsx
+++ src/layouts/function-components/TestimonialSlider.tsx
@@ -30,23 +30,22 @@ const TestimonialCard: FC<TestimonialItem> = (item) => (
     <div className="review-author-avatar bg-gradient">
       <img alt={item.author} className="rounded-full" src={item.avatar} />
     </div>
-    <div className="review-content">
-      <div>
-        <h4 className="mb-2">{item.author}</h4>
-        <p className="text-text-dark/80 mb-4">{item.organization}</p>
-        {item.reference && (
-          <p className="text-text-dark/60 mb-3 text-xs">
-            {item.reference && item.reference.link ? (
-              <a className="hover:text-primary" href={item.reference.link} rel="noopener noreferrer" target="_blank">
-                🌐 {item.reference.displayName}
-              </a>
-            ) : (
-              <span>📍 {item.reference.displayName}</span>
-            )}
-          </p>
-        )}
-        <p className="mb-4">{item.content}</p>
-      </div>
+    <div className="flex flex-col">
+      <h4 className="mb-2">{item.author}</h4>
+      <p className="text-text-dark/80 mb-4">{item.organization}</p>
+      {item.reference && (
+        <p className="text-text-dark/60 mb-3 text-xs">
+          {item.reference && item.reference.link ? (
+            <a className="hover:text-primary-800" href={item.reference.link} rel="noopener noreferrer" target="_blank">
+              🌐 {item.reference.displayName}
+            </a>
+          ) : (
+            <span>📍 {item.reference.displayName}</span>
+          )}
+        </p>
+      )}
+      <p className="mb-4">{item.content}</p>
+
       <div className="review-badge">
         <span className="badge">{item.badge.value}</span>
       </div>
@@ -60,8 +59,9 @@ const TestimonialSlider: FC<TestimonialSliderProps> = ({ list }) => {
   const paginationRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
+    <div className="testimonial-slider">
       <Swiper
+        loop
         modules={[Pagination, Autoplay]}
         slidesPerView={1}
         autoplay={{
@@ -88,19 +88,15 @@ const TestimonialSlider: FC<TestimonialSliderProps> = ({ list }) => {
         }}
       >
         {list.map((item, index) => (
-          <SwiperSlide key={'feature-' + index}>
+          <SwiperSlide key={'testimonial-' + index}>
             <TestimonialCard {...item} />
           </SwiperSlide>
         ))}
       </Swiper>
       <div className="testimonial-slider-pagination">
-        <div
-          ref={paginationRef}
-          className="swiper-pagination !bottom-0"
-          style={{ width: '100%' }}
-        ></div>
+        <div ref={paginationRef} className="swiper-pagination !bottom-0" style={{ width: '100%' }}></div>
       </div>
-    </>
+    </div>
   );
 };
 
