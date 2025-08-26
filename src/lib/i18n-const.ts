d32ada9d4884eac6c496211382873eb5365b1cbf
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-08-26 17:15:17 +0200

    respect current path when switcing languages

--- src/lib/i18n-const.ts
+++ src/lib/i18n-const.ts
@@ -16,3 +16,14 @@ export function getLangFromUrl(url: URL): LanguageCode {
   }
   return defaultLang;
 }
+
+export function getPathWithoutLang(url: URL): string {
+  const segments = url.pathname.split('/').filter(Boolean);
+  const [firstSegment] = segments;
+
+  if (firstSegment in languages) {
+    return segments.length > 1 ? `/${segments.slice(1).join('/')}` : '/';
+  }
+
+  return url.pathname;
+}
