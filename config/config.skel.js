6a7a8ca6b0f88f9cd8ed4bb0436572c93a6823b5
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-09-11 19:32:30 +0200

    fix: use single GTM id for all apps in a given environment

--- config/config.skel.js
+++ config/config.skel.js
@@ -1,5 +1,5 @@
 window.globalConfig = {
   FRONTEND_URL: ${frontendUrl},
   TIER_INFO: ${tierInfo},
-  GOOGLE_TAG_MANAGER_ID: ${landingGoogleTagManagerId}
+  GOOGLE_TAG_MANAGER_ID: ${googleTagManagerId}
 };
