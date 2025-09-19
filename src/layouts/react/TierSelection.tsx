edeb71f743e5d2bd0ba1e5d764875f4a6f89c4c3
Sergio Martin Sanchez <sergio.anger@gmail.com>
2025-09-19 17:53:36 +0200

    feat: pricing calculator

--- src/layouts/react/TierSelection.tsx
+++ src/layouts/react/TierSelection.tsx
@@ -1,8 +1,11 @@
 import { MantineProvider } from '@mantine/core';
-import { TierSelection as TierSelectionReact } from '@notifycal/shared/components';
+import {
+  PricingCalculator as PricingCalculatorReact,
+  TierSelection as TierSelectionReact
+} from '@notifycal/shared/components';
 import { orderedTierInfoWithIcons } from '@notifycal/shared/pricing';
-import type { LanguageCode } from '@notifycal/shared/types';
-import type { JSX } from 'react';
+import type { LanguageCode, TierId } from '@notifycal/shared/types';
+import { useState, type JSX } from 'react';
 
 import { useServiceConfig } from '@/hooks/useServiceConfig';
 import { colors } from '@notifycal/shared/theme';
@@ -12,6 +15,8 @@ interface TierSelection {
 }
 
 export function TierSelection({ locale }: TierSelection): JSX.Element {
+  const [recommendedTier, setTierRecommended] = useState<{ tierId: TierId; trigger: number } | undefined>(undefined);
+
   const serviceConfig = useServiceConfig();
   if (!serviceConfig) {
     return <div>Loading...</div>;
@@ -19,6 +24,12 @@ export function TierSelection({ locale }: TierSelection): JSX.Element {
   const tiers = serviceConfig.TIER_INFO.tiers;
   const frontendUrl = serviceConfig.FRONTEND_URL;
 
+  const onTierSelected = (): Promise<void> => {
+    window.open(frontendUrl, '_blank');
+    return Promise.resolve();
+  };
+
+  const translatedOrderedTierInfoWithIcons = orderedTierInfoWithIcons(tiers, locale);
   return (
     <MantineProvider
       theme={{
@@ -30,12 +41,21 @@ export function TierSelection({ locale }: TierSelection): JSX.Element {
         isCardButtonDisabled={() => false}
         isCardButtonLoading={() => false}
         lang={locale}
-        orderedTierInfoWithIcons={orderedTierInfoWithIcons(tiers, locale)}
-        onTierSelection={() => {
-          window.open(frontendUrl, '_blank');
-          return Promise.resolve();
-        }}
+        orderedTierInfoWithIcons={translatedOrderedTierInfoWithIcons}
+        recommendedTier={recommendedTier}
+        onTierSelection={onTierSelected}
       />
+      <div className="mt-20">
+        <PricingCalculatorReact
+          collapsible
+          defaultExpanded
+          contactUrl={`${frontendUrl}/#/onboarding/feedback`}
+          isSelectButtonLoading={false}
+          orderedTierInfoWithIcons={translatedOrderedTierInfoWithIcons}
+          onTierRecommendation={setTierRecommended}
+          onTierSelect={onTierSelected}
+        />
+      </div>
     </MantineProvider>
   );
 }
